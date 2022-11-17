51e6bf345b91c198fe8049a2b49d813e6008c07f
Ezra <ezraermy@gmail.com>
2022-11-17 23:49:55 +0300

    Fixed linter errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,58 @@
-const hamburger = document.querySelector(".toggle");
-const navMenu = document.querySelector(".menu");
+const hamburger = document.querySelector('.toggle');
+const navMenu = document.querySelector('.menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".menu_item").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('.menu_item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
 
 const projectDetails = [
   {
-    title: "Project Multi post",
+    title: 'Project Multi post',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolorlorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/gymfit.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolorlorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/gymfit.png',
   },
   {
-    title: "Project second",
+    title: 'Project second',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/Snapshoot.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/Snapshoot.png',
   },
   {
-    title: "Project second",
+    title: 'Project second',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/Snapshoot.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/Snapshoot.png',
   },
   {
-    title: "Project second",
+    title: 'Project second',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/Snapshoot.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/Snapshoot.png',
   },
   {
-    title: "Project second",
+    title: 'Project second',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/Snapshoot.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/Snapshoot.png',
   },
   {
-    title: "Project second",
+    title: 'Project second',
     description:
-      "lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor",
-    image: "./assets/images/Snapshoot.png",
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor',
+    image: './assets/images/Snapshoot.png',
   },
 ];
 
-var project_cards = document.querySelector(".project_cards");
+const projectCards = document.querySelector('.project_cards');
 
-project_cards.innerHTML += `
+projectCards.innerHTML += `
 
 <div class="project1">
 <img
@@ -79,8 +77,8 @@ project_cards.innerHTML += `
 </div>
 </div>`;
 
-for (var i = 1; i <= projectDetails.length; i++) {
-  var cards = ` 
+for (let i = 1; i <= projectDetails.length; i += 1) {
+  const cards = ` 
   <div class="project">
   <div class="head_card">
     <h3>Professional Art <br />Printing Data</h3>
@@ -97,38 +95,33 @@ for (var i = 1; i <= projectDetails.length; i++) {
   <button id="project-2" class="popup-btn">See Project</button>
 </div>`;
 
-  project_cards.innerHTML += cards;
+  projectCards.innerHTML += cards;
 }
 
-var pop = document.querySelector(".pop-up");
-var wrapper = document.querySelector(".pop-wrapper");
-function popup_show() {
-  pop.classList.add("show");
-  wrapper.classList.add("show");
+const pop = document.querySelector('.pop-up');
+const wrapper = document.querySelector('.pop-wrapper');
+function popupShow() {
+  pop.classList.add('show');
+  wrapper.classList.add('show');
 }
 
-const proj1 = document.querySelector("#project-1");
-proj1.addEventListener("click", () => {
-  popup_show();
-  document.querySelector(".popup-title").innerHTML = projectDetails[0].title;
-  document.querySelector(".popup-img").src = projectDetails[0].image;
-  document.querySelector(".popup-text").innerHTML = projectDetails[0].description;
+const proj1 = document.querySelector('#project-1');
+proj1.addEventListener('click', () => {
+  popupShow();
+  document.querySelector('.popup-title').innerHTML = projectDetails[0].title;
+  document.querySelector('.popup-img').src = projectDetails[0].image;
+  document.querySelector('.popup-text').innerHTML = projectDetails[0].description;
 });
 
-document.querySelectorAll("#project-2").forEach((p) =>
-  p.addEventListener("click", () => {
-    popup_show();
-    document.querySelector(".popup-title").innerHTML = projectDetails[1].title;
-    document.querySelector(".popup-img").src = projectDetails[1].image;
-    document.querySelector(".popup-text").innerHTML =
-      projectDetails[1].description;
-  })
-);
+document.querySelectorAll('#project-2').forEach((p) => p.addEventListener('click', () => {
+  popupShow();
+  document.querySelector('.popup-title').innerHTML = projectDetails[1].title;
+  document.querySelector('.popup-img').src = projectDetails[1].image;
+  document.querySelector('.popup-text').innerHTML = projectDetails[1].description;
+}));
 
-let closeButton = document.querySelector(".popup-close");
-closeButton.addEventListener("click", () => {
-  pop.classList.remove("show");
-  wrapper.classList.remove("show");
+const closeButton = document.querySelector('.popup-close');
+closeButton.addEventListener('click', () => {
+  pop.classList.remove('show');
+  wrapper.classList.remove('show');
 });
-
-
